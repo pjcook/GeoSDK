acee000f710a7d3490c175d4eccf0886401290b2
PJ <pjcook@users.noreply.github.com>
2019-01-11 10:25:22 +0000

    Initial commit
    Signed-off-by: PJ <pjcook@users.noreply.github.com>

---
+++ GeoSDK/GeoSDK.h
@@ -0,0 +1,19 @@
+//
+//  GeoSDK.h
+//  GeoSDK
+//
+//  Created by PJ COOK on 11/01/2019.
+//  Copyright © 2019 Zappit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoSDK.
+FOUNDATION_EXPORT double GeoSDKVersionNumber;
+
+//! Project version string for GeoSDK.
+FOUNDATION_EXPORT const unsigned char GeoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoSDK/PublicHeader.h>
+
+
